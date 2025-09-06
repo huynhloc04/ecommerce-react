aa8a6991f8b9c4f4a134771701cfb25d4f49e862
edwinhuynh-novobi <edwin.huynh@novobi.com>
2025-09-06 23:01:48 +0700

    [ADD] styling with scss and mixin

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import Header from '@components/Header/Header';
+import MainLayout from '@components/Layout/Layout';
+import Footer from '@components/Footer/Footer';
+import Button from '@components/Button/Button';
 
 function App() {
-  return <> </>;
+  return (
+    <>
+      <MainLayout>
+        {/* <Header />
+        Content MotoGP 2025.
+        <Footer /> */}
+        <Button />
+      </MainLayout>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss';
+
+function Button() {
+  return (
+    <>
+      <button className={styles.btn1}>X</button>
+      <button className={styles.btn2}>Y</button>
+    </>
+  );
+}
+
+export default Button;
